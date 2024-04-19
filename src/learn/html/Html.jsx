f249d0cdc0f1f97c898508f771f134f4e1e65b7c
developer-assets <999patrickobama@gmail.com>
2024-04-20 00:08:54 +0300

    All changes for 19th and before

--- src/learn/html/Html.jsx
+++ src/learn/html/Html.jsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Intro from '../../courses/html/Intro';
-import SyntaxHighlighter from 'react-syntax-highlighter';
-import { anOldHope } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 import Navbar from '../../components/Navbar';
 import HtmlSidebar from '../../components/HtmlSidebar';
 import Footer from '../../components/Footer';
+import { FaCircle } from 'react-icons/fa6';
+import CodeEditor from '../../components/CodeEditor';
 
 const Html = () => {
+  const [expanded, setExpanded] = useState(false);
+
+
+
   return (
     <div className='bg-gray-50 text-black dark:text-white relative dark:bg-gray-950 mx-auto'>
       <div className='container max-w-7xl mx-auto'>
@@ -21,10 +25,26 @@ const Html = () => {
             <HtmlSidebar />
           </div>
         </div>
-        <div className='pt-32 pb-32'>
+        <div className='pt-32 pb-10'>
           <Footer />
         </div>
-        
+      </div>
+      <div className='hidden lg:block'>
+        <div className='w-screen overflow-hidden fixed bg-gray-200 dark:bg-gray-800 border border-yellow-500 dark:border-blue-600 rounded-md z-[10] bottom-0 left-0'>
+          <div className='flex justify-start gap-10 items-center h-8 pl-4'>
+            <button onClick={() => setExpanded(false)} className='text-red-500 cursor-pointer'>
+              <FaCircle />
+            </button>
+            <button onClick={() => setExpanded(true)} className='text-green-500 cursor-pointer'>
+              <FaCircle />
+            </button>
+          </div>
+          <hr className={expanded ? 'h-[1px] w-[97%] mx-auto border-none bg-gray-950 mt-2 mb-4 dark:bg-gray-50' : 'hidden'} />
+          {/* Extended panel body */}
+          <div className={expanded ? 'h-[90vh] duration-300' : 'h-0 duration-300'}>
+            <CodeEditor />
+          </div>
+        </div>
       </div>
     </div>
   );
