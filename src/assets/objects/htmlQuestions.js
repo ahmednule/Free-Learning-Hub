b9940a2431e7acd9810382be15bcc62674b4c8b8
developer-assets <999patrickobama@gmail.com>
2024-04-19 15:45:46 +0300

    Initialized questionaire logic

---
+++ src/assets/objects/htmlQuestions.js
@@ -0,0 +1,117 @@
+export const lessonOneQuestions = [
+  {
+    id: 1,
+    question: "What is the main purpose of HTML?",
+    correct: 1,
+    choices: [
+      {
+        id: 1,
+        answer: "Structure web pages"
+      },
+      {
+        id: 2,
+        answer: "Style web pages"
+      },
+      {
+        id: 3,
+        answer: "Execute scripts"
+      },
+      {
+        id: 4,
+        answer: "Manage databases"
+      }
+    ]
+  },
+  {
+    id: 2,
+    question: "Which language is responsible for styling web pages?",
+    correct: 2,
+    choices: [
+      {
+        id: 1,
+        answer: "HTML"
+      },
+      {
+        id: 2,
+        answer: "CSS"
+      },
+      {
+        id: 3,
+        answer: "JavaScript"
+      },
+      {
+        id: 4,
+        answer: "PHP"
+      }
+    ]
+  },
+  {
+    id: 3,
+    question: "How would you describe the relationship between HTML and CSS?",
+    correct: 3,
+    choices: [
+      {
+        id: 1,
+        answer: "HTML styles elements"
+      },
+      {
+        id: 2,
+        answer: "CSS provides structure"
+      },
+      {
+        id: 3,
+        answer: "HTML creates structure, CSS styles it"
+      },
+      {
+        id: 4,
+        answer: "CSS creates structure, HTML styles it"
+      }
+    ]
+  },
+  {
+    id: 4,
+    question: "What analogy best represents the roles of HTML and CSS?",
+    correct: 1,
+    choices: [
+      {
+        id: 1,
+        answer: "HTML is the frame, CSS is the paint"
+      },
+      {
+        id: 2,
+        answer: "HTML is the paint, CSS is the frame"
+      },
+      {
+        id: 3,
+        answer: "HTML is the architect, CSS is the builder"
+      },
+      {
+        id: 4,
+        answer: "HTML is the builder, CSS is the architect"
+      }
+    ]
+  },
+  {
+    id: 5,
+    question: "What benefit do developers gain from mastering HTML and CSS?",
+    correct: 3,
+    choices: [
+      {
+        id: 1,
+        answer: "Creating interactive animations"
+      },
+      {
+        id: 2,
+        answer: "Building complex databases"
+      },
+      {
+        id: 3,
+        answer: "Crafting visually appealing web pages"
+      },
+      {
+        id: 4,
+        answer: "Writing server-side scripts"
+      }
+    ]
+  }
+];
