1a355be633eec6073d5e7fd0967abc699cac97f1
developer-assets <999patrickobama@gmail.com>
2024-04-19 09:45:36 +0300

    UI updates

--- src/components/Contributors.jsx
+++ src/components/Contributors.jsx
@@ -36,7 +36,7 @@ const Contributors = () => {
         {contributers.map(user => (
           <div key={user.id} className='p-4'>
             <a href={user.html_url} className='cursor-pointer' >
-              <img src={user.avatar_url} className='w-24 mx-auto h-24 rounded-full object-cover' alt={`Profile Picture of ${user.login}`} />
+              <img src={user.avatar_url} className='w-20 mx-auto h-20 rounded-full object-cover' alt={`Profile Picture of ${user.login}`} />
             </a>
             <p className='text-sm text-center mt-1 text-orange-500 hover:underline dark:text-blue-600'>
               <a href={user.html_url} target="_blank" rel="noopener noreferrer">{user.login}</a>
