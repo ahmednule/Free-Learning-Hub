998580ec67981a5d0ae6cc457c1b1b957ef967a9
Patrick Obama <999patrickobama@gmail.com>
2024-05-04 16:15:49 +0300

    Custom scrollbar

--- client/src/Components/Home/Hero.jsx
+++ client/src/Components/Home/Hero.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className='flex flex-col items-center justify-center h-[699px] gap-5'>
+      <div className='w-full'>
+        <img src="" alt="Free Learning Hub Logo" className='h-52 w-fit mx-auto' />
+      </div>
+    </div>
   )
 }
 
