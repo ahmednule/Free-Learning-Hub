f61266c08934f81a0b6f557d7cc3e44c1648a104
Patrick Obama <999patrickobama@gmail.com>
2024-05-04 16:54:42 +0300

    Set up scrollbar

--- client/src/Components/Home/Hero.jsx
+++ client/src/Components/Home/Hero.jsx
@@ -4,7 +4,7 @@ const Hero = () => {
   return (
     <div className='flex flex-col items-center justify-center h-[699px] gap-5'>
       <div className='w-full'>
-        <img src="" alt="Free Learning Hub Logo" className='h-52 w-fit mx-auto' />
+        <img src="./logobig.webp" alt="Free Learning Hub Logo" className='h-52 w-fit mx-auto' />
       </div>
     </div>
   )
