f1797962d2c89023eb22c0b59f9a969d660ddea7
Patrick Obama <999patrickobama@gmail.com>
2024-05-06 19:08:15 +0300

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,29 +1,16 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
-export default defineConfig(({ command }) => {
-  const isProduction = command === 'build'; // Check if it's a production build
-
-  const devProxy = {
-    '/api': {
-      target: 'http://localhost:3000',
-      changeOrigin: true,
-      rewrite: (path) => path
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: { // Set up proxy
+    proxy: {
+      '/api': {
+        target: 'https://free-learning-hub-backend.vercel.app',
+        changeOrigin: true,
+        rewrite: (path) => path
+      }
     }
-  };
-
-  const prodProxy = {
-    '/api': {
-      target: 'https://free-learning-hub-backend.vercel.app',
-      changeOrigin: true,
-      rewrite: (path) => path.startsWith('/api') ? path.replace('/api', '') : path
-    }
-  };
-
-  return {
-    plugins: [react()],
-    server: {
-      proxy: isProduction ? prodProxy : devProxy
-    }
-  };
-});
+  },
+})
