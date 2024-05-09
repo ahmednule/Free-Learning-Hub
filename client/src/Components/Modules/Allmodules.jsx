1812832e51f871c5889fe413d0d8b54519e167ed
Patrick Obama <999patrickobama@gmail.com>
2024-05-09 21:38:57 +0300

    Add user modules and all modules

---
+++ client/src/Components/Modules/Allmodules.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import { allModules } from '../../Data/Modules.js'
+import { FaAngleRight } from 'react-icons/fa6';
+import { PiFolderStarThin } from 'react-icons/pi';
+import { Link } from 'react-router-dom';
+
+const Allmodules = () => {
+  
+
+
+
+  return (
+    <div>
+      <div className="grid lg:grid-cols-2 gap-3 mt-14 mb-36">
+      {allModules.map((lang) => {
+        return (
+          <div key={lang.id} className="w-full bg-blue-500/5 border border-blue-500/15 rounded-2xl hover:bg-blue-500/10 hover:border-blue-500/20 duration-200"  >
+          <div className=" cursor-pointer flex p-4">
+            <div className="hidden lg:flex-[0.15] lg:flex items-center justify-center">
+              <div className="w-14 h-14 bg-blue-500/35 rounded-full flex items-center justify-center text-white">
+                <PiFolderStarThin size={30} />
+              </div>
+            </div>
+            <div className="lg:flex-[0.85] flex-1 flex flex-col justify-start gap-2">
+              <p className="font-semibold text-left text-xl"> {lang.name} </p>
+              <p className="text-xs lg:text-sm"> {lang.description} </p>
+              <Link to={`/learn/register/${lang.id}`}>
+                <button className="mr-auto flex items-center gap-[5px] text-blue-500 group">
+                  <span>Get Started</span>
+                  <FaAngleRight className="group-hover:translate-x-1 duration-200 mt-[1px]" />
+                </button>
+              </Link>
+            </div>
+          </div>
+
+            <div className='w-[90%] bg-gray-800 rounded-md h-12 mx-auto mb-4 border border-gray-700'>
+              <div className='min-w-[30px] w-fit bg-lime-600 h-full rounded-md'>
+                <p className='text-gray-950 pt-[10px] font-semibold text-center'>
+                  0%
+                </p>
+              </div>
+            </div>
+          </div>
+        )
+      })}
+      
+    </div>
+    </div>
+  )
+}
+
+export default Allmodules
---
+++ client/src/Components/Modules/Mymodules.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Mymodules = () => {
+  return (
+    <div>Mymodules</div>
+  )
+}
+
+export default Mymodules
