53af2f95bf5172ab91ada58a0ed9f2a92aac03bb
Patrick Obama <999patrickobama@gmail.com>
2024-05-10 04:58:17 +0300

    Fix subfooter positioning

--- client/src/Components/Modules/Subfooter.jsx
+++ client/src/Components/Modules/Subfooter.jsx
@@ -8,7 +8,7 @@ const Subfooter = (props) => {
   return (
     <div>
       <div className='flex flex-col md:flex-row w-full gap-3 md:justify-between'>
-        <Link to={l1} className='block w-full'>
+        <Link to={l1} className='flex'>
           <button className='flex group justify-center w-full md:w-fit duration-200 transition-all bg-blue-500/15 border border-blue-500/25 hover:bg-blue-500/20 hover:border-blue-500/30 py-2 rounded-lg px-5 gap-2'>
             <span>
               <FaAngleRight className='group-hover:-translate-x-[5px] duration-200 rotate-180 mt-1' />
@@ -16,7 +16,7 @@ const Subfooter = (props) => {
             <span> {t1} </span>
           </button>
         </Link>
-        <Link to={l2} className='block w-full'>
+        <Link to={l2} className='flex'>
           <button className='flex group justify-center w-full md:w-fit duration-200 transition-all bg-blue-500/15 border border-blue-500/25 hover:bg-blue-500/20 hover:border-blue-500/30 py-2 rounded-lg px-5 gap-2'>
             <span> {t2} </span>
             <span>
