35e2337960bd2ab87615204934fc1c4eb5c3a90e
developer-assets <999patrickobama@gmail.com>
2024-05-12 00:16:24 +0300

    Updated profile dashboard

--- client/src/Components/Profile/Dashboard.jsx
+++ client/src/Components/Profile/Dashboard.jsx
@@ -1,9 +1,10 @@
 import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 import placeHolderImage from '../../Images/userImage.jpg'
-import { deleteUserCookie, fetchUserDataFromCookie } from '../../Helpers/handlecookie';
-import Axios from 'axios';
-import toast from 'react-hot-toast';
+import { fetchUserDataFromCookie } from '../../Helpers/handlecookie';
+import { CiUser } from 'react-icons/ci';
+import { FaUserCog } from 'react-icons/fa';
+import { MdOutlineEmail } from 'react-icons/md';
 
 const Dashboard = () => {
   const navigate = useNavigate();
@@ -26,28 +27,33 @@ const Dashboard = () => {
   }, []);
 
   return (
-    <div className='w-full flex flex-col lg:flex-row gap-4'>
-      <div className='w-[400px] mx-auto px-3 bg-gray-950 rounded-xl pb-8'>
-        <img src={hasPhoto ? onlinePhotoURL : placeHolderImage} className='w-[320px] mx-auto mt-4 h-[250px] rounded-2xl object-cover' alt="User Profile Picture" />
-        <p className='mt-5 pl-5 font-semibold text-2xl'> {userInfo.fullName} </p>
-        <div className='mt-10 border-b border-gray-700 w-full'>
-          <p className='pl-5 text-green-500'> {userInfo.username} </p>
-        </div>
-        <div className='mt-4 border-b border-gray-700 w-full'>
-          <p className='pl-5'> {userInfo.email} </p>
-        </div>
-        
-      </div>
-      <div className='w-full'>
-        <div className='w-full h-44 bg-gray-950 rounded-xl'>
-        </div>
-        <div className='w-full mt-5 h-44 bg-gray-950 rounded-xl'>
+    <div className='w-full flex justify-center'>
+      <div className='rounded-xl bg-gray-950 p-2'>
 
+        <div className='h-64 relative w-[360px] rounded-t rounded-[80%] bg-gradient-to-br from-blue-600 to-cyan-600'>
+          <div className='absolute bottom-0 left-0 w-full'>
+            <img src={hasPhoto ? onlinePhotoURL : placeHolderImage} alt="User Profile Picture" className='rounded-full w-36 mx-auto translate-y-14 border border-gray-300 h-36 object-cover' />
+          </div>
+        </div>
+
+        <div className='mt-28 mb-10 text-xl flex flex-col gap-3'>
+          <div className='flex justify-start items-center pl-2 gap-5 border-b border-gray-700 py-3'>
+            <CiUser size={30} />
+            <p>{userInfo.fullName}</p>
+          </div>
+          <div className='flex justify-start items-center pl-2 gap-5 border-b border-gray-700 py-3'>
+            <FaUserCog size={30} />
+            <p>{userInfo.username}</p>
+          </div>
+          <div className='flex justify-start items-center pl-2 gap-5 border-b border-gray-700 py-3'>
+            <MdOutlineEmail size={30} />
+            <p>{userInfo.email}</p>
+          </div>
         </div>
 
       </div>
     </div>
-  )
+  );
 }
 
-export default Dashboard
+export default Dashboard;
--- client/src/Pages/Profile.jsx
+++ client/src/Pages/Profile.jsx
@@ -3,8 +3,6 @@ import { Link, useNavigate } from 'react-router-dom'
 import Dashboard from '../Components/Profile/Dashboard'
 import Account from '../Components/Profile/Account'
 import { fetchUserDataFromCookie } from '../Helpers/handlecookie'
-import { MdSpaceDashboard, MdSupportAgent } from 'react-icons/md';
-import { RiAccountPinCircleFill } from 'react-icons/ri';
 
 const Profile = () => {
   const navigate = useNavigate();
@@ -22,32 +20,26 @@ const Profile = () => {
 
   return (
     <div>
-      <div className='pt-16'>
+      <div>
         <div className='border-b border-gray-700 py-3 pl-4'>
           <h2 className='text-xl md:text-2xl lg:text-4xl'>Welcome back.... </h2>
           <p className='mt-3 italic'>Pick up right where you left or pick a module to start learning from <Link className='linkOne' to={'/learn'}>here</Link>.</p>
         </div>
 
-        <div className='flex'>
-          <div className='hidden md:block flex-[0.25] min-h-screen'>
-            <div onClick={() => setActiveTab(1)} className={activeTab === 1 ? 'pt-4 pb-2 flex cursor-pointer justify-start items-center pl-2 bg-gray-800 border-b border-gray-700 gap-3' : 'pt-4 pb-2 flex cursor-pointer justify-start items-center pl-2 border-b border-gray-700 gap-3'}>
-              <MdSpaceDashboard />
-              <span>Dashboard</span>
-            </div>
-            <div onClick={() => setActiveTab(2)} className={activeTab === 2 ? 'pt-4 pb-2 flex cursor-pointer justify-start items-center pl-2 bg-gray-800 border-b border-gray-700 gap-3' : 'pt-4 pb-2 flex cursor-pointer justify-start items-center pl-2 border-b border-gray-700 gap-3'}>
-              <RiAccountPinCircleFill />
-              <span>Account</span>
-            </div>
-            <div onClick={() => setActiveTab(3)} className={activeTab === 3 ? 'pt-4 pb-2 flex cursor-pointer justify-start items-center pl-2 bg-gray-800 border-b border-gray-700 gap-3' : 'pt-4 pb-2 flex cursor-pointer justify-start items-center pl-2 border-b border-gray-700 gap-3'}>
-              <MdSupportAgent />
-              <span>Support</span>
-            </div>
+        <div className='h-10 w-full border-b sticky z-10 top-14 bg-gray-950 border-gray-700 flex items-center justify-evenly'>
+          <div onClick={() => setActiveTab(1)} className={activeTab === 1 ? 'bg-gray-800 cursor-pointer w-full h-full' : 'w-full cursor-pointer h-full'}>
+            <button className='pt-[6px] w-full mx-auto font-semibold'>DASHBOARD</button>
           </div>
-          <div className='border-l border-gray-700 bg-gray-900 flex-1 md:flex-[0.75] p-5'>
-            {activeTab === 1 && <Dashboard data={userInfo} />}
-            {activeTab === 2 && <Account data={userInfo} />}
+          <div onClick={() => setActiveTab(2)} className={activeTab === 2 ? 'bg-gray-800 cursor-pointer w-full h-full' : 'w-full cursor-pointer h-full'}>
+            <button className='pt-[6px] w-full mx-auto font-semibold'>ACCOUNT</button>
           </div>
         </div>
+
+        <div className='w-full bg-gray-800 pt-5 pb-20'>
+          {activeTab === 1 && <Dashboard data={userInfo} />}
+          {activeTab === 2 && <Account data={userInfo} />}
+        </div>
+
       </div>
     </div>
   );
